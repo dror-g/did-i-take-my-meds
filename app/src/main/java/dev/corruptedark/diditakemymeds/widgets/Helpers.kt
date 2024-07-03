b35f84a270d7f5ca43f20535c157e67d0089b9cb
kreiri <>
2024-07-03 18:32:19 +0300

    working on widget shared preference methods

--- app/src/main/java/dev/corruptedark/diditakemymeds/widgets/Helpers.kt
+++ app/src/main/java/dev/corruptedark/diditakemymeds/widgets/Helpers.kt
@@ -3,6 +3,7 @@ package dev.corruptedark.diditakemymeds.widgets
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.widget.RemoteViews
 import dev.corruptedark.diditakemymeds.R
 import dev.corruptedark.diditakemymeds.data.db.medicationDao
@@ -68,36 +69,45 @@ internal fun updateAppWidget(
 }
 
 internal fun saveLayoutPref(context: Context, appWidgetId: Int, layoutId: Int) {
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$PREF_KEY_LAYOUT"
-    context.getSharedPreferences(PREFS_NAME, 0).edit().putInt(prefName, layoutId).apply()
+    val prefName = getLayoutPrefKey(appWidgetId)
+    getSharedPreferences(context).edit().putInt(prefName, layoutId).apply()
 }
 
 internal fun deleteLayoutPref(context: Context, appWidgetId: Int) {
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$PREF_KEY_LAYOUT"
-    context.getSharedPreferences(PREFS_NAME, 0).edit().remove(prefName).apply()
+    val prefName = getLayoutPrefKey(appWidgetId)
+    getSharedPreferences(context).edit().remove(prefName).apply()
 }
 
 internal fun getLayoutPref(context: Context, appWidgetId: Int): Int {
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$PREF_KEY_LAYOUT"
-    return context.getSharedPreferences(PREFS_NAME, 0)
+    val prefName = getLayoutPrefKey(appWidgetId)
+    return getSharedPreferences(context)
             .getInt(prefName, R.layout.simple_single_med_widget_dark)
 }
 
-
 internal fun saveMedIdPref(context: Context, appWidgetId: Int, medId: Long) {
-    val prefKey = context.applicationContext.getString(R.string.med_id_key)
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$prefKey"
-    context.getSharedPreferences(PREFS_NAME, 0).edit().putLong(prefName, medId).apply()
+    val prefName = getMedIdPrefKey(context, appWidgetId)
+    getSharedPreferences(context).edit().putLong(prefName, medId).apply()
 }
 
 internal fun loadMedIdPref(context: Context, appWidgetId: Int): Long {
-    val prefKey = context.applicationContext.getString(R.string.med_id_key)
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$prefKey"
-    return context.getSharedPreferences(PREFS_NAME, 0).getLong(prefName, Medication.INVALID_MED_ID)
+    val prefName = getMedIdPrefKey(context, appWidgetId)
+    return getSharedPreferences(context).getLong(prefName, Medication.INVALID_MED_ID)
 }
 
 internal fun deleteMedIdPref(context: Context, appWidgetId: Int) {
+    val prefName = getMedIdPrefKey(context, appWidgetId)
+    getSharedPreferences(context).edit().remove(prefName).apply()
+}
+
+private fun getMedIdPrefKey(context: Context, appWidgetId: Int): String {
     val prefKey = context.applicationContext.getString(R.string.med_id_key)
-    val prefName = "$PREF_PREFIX_KEY$appWidgetId$prefKey"
-    context.getSharedPreferences(PREFS_NAME, 0).edit().remove(prefName).apply()
+    return "$PREF_PREFIX_KEY$appWidgetId$prefKey"
+}
+
+private fun getLayoutPrefKey(appWidgetId: Int): String {
+    return "$PREF_PREFIX_KEY$appWidgetId$PREF_KEY_LAYOUT"
+}
+
+private fun getSharedPreferences(context: Context): SharedPreferences {
+    return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 }
